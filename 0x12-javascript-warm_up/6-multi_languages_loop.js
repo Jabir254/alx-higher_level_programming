296d176bd3b51c07fc1bc3e6c8f112b10f714546
jabir254 <jabirkullow@gmail.com>
2023-04-13 09:57:54 +0000

    prints x times "C is fun"

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-for (const word of ['C is fun', 'Python is cool', 'Javascript is amazing',])
-{
-	console.log(word);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
