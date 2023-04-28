05d941fc2254ddee87d7e9de6dbaa77b21294fe6
jabir254 <jabirkullow@gmail.com>
2023-04-28 20:39:24 +0300

    python scripts that fetches data

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,15 +2,15 @@
 """
 a python script that fetches data and display.
 """
+if __name__ == '__main__':
+    import urllib.request
 
-import urllib.request
+    url = 'https://alx-intranet.hbtn.io/status'
 
-url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-
-print('- Body response:')
-print('\t- type:', type(html))
-print('\t- content:', html)
-print('\t- utf8 content:', html.decode('utf-8'))
+        print('- Body response:')
+        print('\t- type:', type(html))
+        print('\t- content:', html)
+        print('\t- utf8 content:', html.decode('utf-8'))
